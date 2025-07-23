806883a1729201cc1ed62c780ff6bfb1358672a9
truant831 <innoegor22@gmail.com>
2025-07-24 01:32:10 +0300

    Add files via upload

---
+++ C++/test.cpp
@@ -0,0 +1,17 @@
+#include <fstream> //для работы с файлами как потоками
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+#ifdef DEBUG
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+#define cin fin
+#define cout fout
+#endif // DEBUG
+
+int main()
+{
+    cout<<"Hello World!";
+}
